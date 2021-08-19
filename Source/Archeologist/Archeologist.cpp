2695ec456ad26fcbd8fd7d633bd3d90768352ec0
yadyginid <yadyginid@gmail.com>
2021-08-19 13:51:54 +0300

    init

---
+++ Source/Archeologist/Archeologist.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Archeologist.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Archeologist, "Archeologist" );
---
+++ Source/Archeologist/ArcheologistGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArcheologistGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCHEOLOGIST_API AArcheologistGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
